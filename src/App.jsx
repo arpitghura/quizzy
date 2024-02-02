3c8f07122c586e19c634228628f92a9f9c594f92
arpitghura <ghurarpit110@gmail.com>
2024-02-03 01:28:47 +0530

    set up basic routing and initial files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import { useState } from "react";
+import HomePage from "./comps/HomePage";
+
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
+import QuizPage from "./comps/QuizPage";
+import ResultPage from "./comps/ResultPage";
 
 function App() {
-  return <>Hello Welcome to Quiz App</>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/result" element={<ResultPage />} />
+          <Route path="/quiz" element={<QuizPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
