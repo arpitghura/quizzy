3c3729a63f78456bb6bb3cd09b52e9ee6fd4ba3c
arpitghura <ghurarpit110@gmail.com>
2024-02-03 02:26:49 +0530

    developed result page layout

--- src/comps/ResultPage.jsx
+++ src/comps/ResultPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ResultPage = () => {
-  return <div>ResultPage</div>;
+const ResultPage = ({ score = 0, totalScore = 0 }) => {
+  return (
+    <div className="flex flex-col justify-center items-center h-[100vh]">
+      <h1 className="font-bold text-4xl p-2">
+        Your Score: {score}/{totalScore}
+      </h1>
+      <p className="text-lg">All the best for the next time!!</p>
+      <Link
+        className="py-2 px-6 bg-blue-500 text-white font-medium text-xl rounded-lg mt-4"
+        to="/"
+      >
+        Go Home
+      </Link>
+    </div>
+  );
 };
-
 export default ResultPage;
